f93e75955adb7f65d79de94130b574675238d02b
Bart Wijnants <bart.w@sparkcentral.com>
2020-01-01 10:12:28 +0100

    Do believe the hype

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { graphql } from "gatsby";
+import rehypeReact from "rehype-react";
 import Layout from "../components/layout";
+
+const Paragraph = ({ children }) => <p className="mt-2">{children}</p>;
+
+const ExternalLink = ({ href, children }) => (
+  <a
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="underline"
+  >
+    {children}
+  </a>
+);
+
+const Subtitle = ({ children }) => (
+  <h2 className="mt-2 font-semibold">{children}</h2>
+);
+
+const renderAst = new rehypeReact({
+  createElement: React.createElement,
+  components: { p: Paragraph, a: ExternalLink, h2: Subtitle }
+}).Compiler;
 
 export default ({
   data: {
     markdownRemark: {
       frontmatter: { title, date, formattedDate },
-      html
+      htmlAst
     }
   }
 }) => {
@@ -17,7 +40,7 @@ export default ({
           {formattedDate}
         </time>
         <h1 className="font-bold text-lg">{title}</h1>
-        <div className="mt-2" dangerouslySetInnerHTML={{ __html: html }} />
+        <div className="mt-2">{renderAst(htmlAst)}</div>
       </div>
     </Layout>
   );
@@ -31,7 +54,7 @@ export const query = graphql`
         date
         formattedDate: date(formatString: "MMMM Do, YYYY")
       }
-      html
+      htmlAst
     }
   }
 `;
